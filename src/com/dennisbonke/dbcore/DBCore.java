30cc15d386136def2995b31d28dd079c9c1f840a
Dennisbonke <dennisb@kabelfoon.net>
2015-01-19 17:28:35 +0100

    Added Chat.

--- src/com/dennisbonke/dbcore/DBCore.java
+++ src/com/dennisbonke/dbcore/DBCore.java
@@ -1,5 +1,6 @@
 package com.dennisbonke.dbcore;
 
+import com.dennisbonke.dbcore.util.Chat;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -7,8 +8,22 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class DBCore extends JavaPlugin {
 
+    public static Chat chat = new Chat("DBCORE");
+    private static DBCore plugin;
+
+    public static Chat getChat(){
+        return chat;
+    }
+
+    public static DBCore getPlugin(){
+        return plugin;
+    }
+
     @Override
     public void onEnable() {
+        chat.sendConsoleMessage("Starting up DBCore, Please wait....");
+
+        plugin = this;
 
     }
 
