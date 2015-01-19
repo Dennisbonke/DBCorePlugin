c31c07a92d2c9f80c968139a0307200d35753f24
Dennisbonke <dennisb@kabelfoon.net>
2015-01-19 17:21:11 +0100

    Added Main Files.

---
+++ src/com/dennisbonke/dbcore/DBCore.java
@@ -0,0 +1,19 @@
+package com.dennisbonke.dbcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Dennisbonke on 19-1-2015.
+ */
+public class DBCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
